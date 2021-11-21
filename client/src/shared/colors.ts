64ff5ded9da86b1ca4e3973be2bec0f696e444b7
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-11-21 20:11:52 +0900

    ✅Add: shared folder and colors.ts -20211121

---
+++ client/src/shared/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  white: '#FFFFF',
+  darkGray: '#3C3C3C',
+  gray: '868686',
+  lightGray: 'DDDDDD',
+  ivory: 'F5F5F5',
+  blue: '#006FAD',
+  red: '#AD0000',
+  yellow: '#FEE500',
+};
