8e652621276198105f8265d537a59b66d118e2be
oh min seok <oh930428@gmail.com>
2021-11-21 17:28:54 +0900

    ✅Add: CustomButton components and install styled-compoent -20211121

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/CustomButton/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="버튼"
+        buttonColor="#006FAD"
+        titleColor="#fff"
+        buttonStyle={{ width: '100%', alignSelf: 'center' }}
+        titleStyle={{ fontSize: 20 }}
+        onPress={() => console.log('good')}
+      />
     </View>
   );
 }
