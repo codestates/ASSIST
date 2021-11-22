f0b357e50756166b6adc76c017a0b16e517075d9
oh min seok <oh930428@gmail.com>
2021-11-22 14:22:11 +0900

    :wrench: Config: modify  file name colors.ts to index.ts and file location

---
+++ client/src/theme/index.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#3C3C3C',
+  darkGray: '#3C3C3C',
+  gray: '#868686',
+  lightGray: '#DDDDDD',
+  ivory: '#F5F5F5',
+  blue: '#006FAD',
+  red: '#AD0000',
+  yellow: '#FEE500',
+};
+
+export const fonts = {
+  primary: 'Spoqa Han Sans Neo',
+};
