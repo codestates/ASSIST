1be616ce87333c34bc1856397a893e3c161c5c6c
shpk93 <shpk930924@gmail.com>
2021-11-23 02:18:26 +0900

    ✅Add:api-send sms auth -20211122

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SmsRepository } from './sms.repository';
+import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
 
-@Module({})
+import { UserService } from './user.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserRepository, SmsRepository])],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UserModule {}
