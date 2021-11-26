94a8037dd646c8aa97686d7ac8e193b0fba20583
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-11-26 18:21:12 +0900

    :recycle: Refactor: createTeam-components -211126

---
+++ client/src/components/view/FinishPageView.tsx
@@ -0,0 +1,40 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import styled from 'styled-components/native';
+import NextButton from '../../components/button/NextButton';
+import MainTitle from '../../components/text/MainTitle';
+import NextPageView from '../../components/view/NextPageView';
+import { colors } from '../../theme/colors';
+
+const Container = styled.View`
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CheckMark = styled.View`
+  margin-bottom: 30px;
+`;
+
+type FinishPageViewProps = {
+  children: React.ReactNode | React.ReactNode[];
+  buttonText?: string;
+  onPress: () => void;
+};
+
+export default function FinishPageView({ children, buttonText, onPress }: FinishPageViewProps) {
+  return (
+    <>
+      <NextPageView>
+        <Container>
+          <CheckMark>
+            <MaterialIcons name="check-circle" size={150} color={colors.blue} />
+          </CheckMark>
+          <MainTitle>{children}</MainTitle>
+        </Container>
+      </NextPageView>
+      <NextButton text={buttonText || '팀 화면으로 이동  >'} onPress={onPress} />
+    </>
+  );
+}
--- client/src/screens/create-team/CreateTeam_6.tsx
+++ client/src/screens/create-team/CreateTeam_6.tsx
@@ -1,43 +1,17 @@
-import { MaterialIcons } from '@expo/vector-icons';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import React from 'react';
-import styled from 'styled-components/native';
-import NextButton from '../../components/button/NextButton';
-import MainTitle from '../../components/text/MainTitle';
-import NextPageView from '../../components/view/NextPageView';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
-import { colors } from '../../theme/colors';
 import { BoldText, LightText } from '../../components/text/SharedText';
-
-const Container = styled.View`
-  flex: 1;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CheckMark = styled.View`
-  margin-bottom: 30px;
-`;
+import FinishPageView from '../../components/view/FinishPageView';
 
 export default function CreateTeamSix() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   return (
-    <>
-      <NextPageView>
-        <Container>
-          <CheckMark>
-            <MaterialIcons name="check-circle" size={150} color={colors.blue} />
-          </CheckMark>
-          <MainTitle>
-            <>
-              <BoldText size={20}>팀 생성이 완료</BoldText>
-              <LightText size={20}>되었어요!</LightText>
-            </>
-          </MainTitle>
-        </Container>
-      </NextPageView>
-      <NextButton text="팀 화면으로 이동  &gt;" onPress={() => navigation.navigate('Home')} />
-    </>
+    <FinishPageView onPress={() => navigation.navigate('Home')}>
+      <>
+        <BoldText size={20}>팀 생성이 완료</BoldText>
+        <LightText size={20}>되었어요!</LightText>
+      </>
+    </FinishPageView>
   );
 }
