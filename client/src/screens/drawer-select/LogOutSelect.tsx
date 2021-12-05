6556b0dc31b7932ecf24273f2f5a18cce7f08919
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-12-05 14:18:55 +0900

    ✅Add: MyPage LogOut -211205

---
+++ client/src/screens/drawer-select/LogOutSelect.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import BottomDrawer from '../../components/drawer/BottomDrawer';
+import { Bold, Regular } from '../../theme/fonts';
+import CommonModalButton from '../../components/button/CommonModalButton';
+
+const TitleContainer = styled.View`
+  margin: 15px 0px;
+`;
+
+const SubtitleContainer = styled.View`
+  margin: 10px 0px 30px 0px;
+`;
+
+const Wrapper = styled.View`
+  padding: 0px 10px 25px 10px;
+`;
+
+export default function LogOutSelect() {
+  return (
+    <BottomDrawer>
+      <Wrapper>
+        <TitleContainer>
+          <Bold size={20}>로그아웃</Bold>
+        </TitleContainer>
+        <SubtitleContainer>
+          <Regular gray>어시스트에서 로그아웃 하시겠습니까?</Regular>
+        </SubtitleContainer>
+        <CommonModalButton color="blue" text="로그아웃  >" onPress={() => console.log('logout')} />
+      </Wrapper>
+    </BottomDrawer>
+  );
+}
