8ca3889b8b956af7e959e5f92c1d4b5ebd77ebbd
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-12-12 17:04:56 +0900

    ✅Add: team select and fetch team data in home -211212

--- client/src/components/header/BottomContainer.tsx
+++ client/src/components/header/BottomContainer.tsx
@@ -5,6 +5,9 @@ import { colors } from '../../theme/colors';
 import { Bold } from '../../theme/fonts';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/reducers';
+import CaptainMark from '../mark/CaptainMark';
 
 const Container = styled.View`
   flex-direction: row;
@@ -15,27 +18,32 @@ const Container = styled.View`
 const TeamSelector = styled.TouchableOpacity`
   flex-direction: row;
   align-items: center;
+  justify-items: center;
   margin-right: 15px;
 `;
 
 const TeamName = styled(Bold)`
   color: ${colors.blue};
   font-size: 17px;
-  margin-right: 1px;
+  margin-right: 5px;
 `;
 
-type BottomContainerProps = {
-  name?: string;
-};
-
-export default function BottomContainer({ name }: BottomContainerProps) {
+export default function BottomContainer() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const { name, leader, id } = useSelector((state: RootState) => state.userReducer.selectedTeam);
   return (
     <Container>
-      <TeamSelector onPress={() => navigation.navigate('TeamSelect')}>
-        <TeamName>{name || '+ 팀 추가'}</TeamName>
-        <MaterialIcons name="keyboard-arrow-down" size={26} color={colors.blue} />
-      </TeamSelector>
+      {id >= 0 ? (
+        <TeamSelector onPress={() => navigation.navigate('TeamSelect')}>
+          <TeamName>{name}</TeamName>
+          {leader && <CaptainMark size="small" />}
+        </TeamSelector>
+      ) : (
+        <TeamSelector onPress={() => navigation.navigate('CreateTeam')}>
+          <TeamName>+ 팀 추가</TeamName>
+          <MaterialIcons name="keyboard-arrow-down" size={26} color={colors.blue} />
+        </TeamSelector>
+      )}
       <TeamSelector>
         <TeamName style={{ color: colors.lightGray }}>용병활동</TeamName>
       </TeamSelector>
--- client/src/screens/drawer-select/TeamSelect.tsx
+++ client/src/screens/drawer-select/TeamSelect.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import BottomDrawer from '../../components/drawer/BottomDrawer';
 import { colors } from '../../theme/colors';
@@ -7,6 +8,11 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
 import CaptainMark from '../../components/mark/CaptainMark';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/reducers';
+import { ASSIST_SERVER_URL } from '@env';
+import axios, { AxiosResponse } from 'axios';
+import { getSelectedTeam, SelectedTeamType } from '../../store/actions/userAction';
 
 const TitleContainer = styled.View`
   margin: 15px 0px;
@@ -41,29 +47,56 @@ const Check = styled.View`
   margin-left: 20px;
 `;
 
+type UserTeams = Array<{ id: number; name: string; leader: boolean }>;
+
 export default function TeamSelect() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const dispatch = useDispatch();
+  const { token, selectedTeam } = useSelector((state: RootState) => state.userReducer);
+  const [teams, setTeams] = useState<UserTeams>([]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getUserTeams().catch((error) => console.log(error));
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  const getUserTeams = async () => {
+    try {
+      const { data }: AxiosResponse<UserTeams> = await axios.get(`${ASSIST_SERVER_URL}/user/team`, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      setTeams(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const goToTeam = ({ id, name, leader }: SelectedTeamType) => {
+    dispatch(getSelectedTeam({ id, name, leader }));
+    navigation.navigate('Home');
+  };
+
   return (
     <BottomDrawer>
       <TitleContainer>
         <Title>팀 선택</Title>
       </TitleContainer>
-      <TeamContainer>
-        <Team>FC 프론트</Team>
-        <IconContainer>
-          <CaptainMark />
-          <Check>
-            <MaterialIcons name="check" size={20} color={colors.blue} />
-          </Check>
-        </IconContainer>
-      </TeamContainer>
-      <TeamContainer>
-        <Team>FC 백엔드</Team>
-      </TeamContainer>
-      <TeamContainer>
-        <Team>FC 살쾡이</Team>
-      </TeamContainer>
-      <TeamContainer onPress={() => navigation.navigate('Home')}>
+      {teams.map((team) => (
+        <TeamContainer key={team.id} onPress={() => goToTeam(team)}>
+          <Team>{team.name}</Team>
+          <IconContainer>
+            {team.leader && <CaptainMark />}
+            {team.id === selectedTeam.id && (
+              <Check>
+                <MaterialIcons name="check" size={20} color={colors.blue} />
+              </Check>
+            )}
+          </IconContainer>
+        </TeamContainer>
+      ))}
+      <TeamContainer onPress={() => navigation.navigate('CreateTeam')}>
         <NewTeam>+ 새로운 소속팀</NewTeam>
       </TeamContainer>
     </BottomDrawer>
